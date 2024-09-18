6051b784ba4199aea3146b425021024654e1b89e
Shahriar Gholami <120596560+shahriar-gholami@users.noreply.github.com>
2024-09-18 09:37:19 +0330

    index redirect to admin page

--- core/core/urls.py
+++ core/core/urls.py
@@ -22,6 +22,8 @@ from django.views.generic import TemplateView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.generic import RedirectView
+
 
 # تنظیمات Swagger و ReDoc
 schema_view = get_schema_view(
@@ -38,6 +40,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=True)),
     path('admin/', admin.site.urls),
     path("info/", include("info.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
